9db347882a6aa1fa384c5c94d0dfded61fd613bc
juliandeveloper05 <juliansoto.dev@email.com>
2024-04-29 19:49:05 -0300

    Updated HamburguerIcon

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,8 @@
-// @/app/ui/fonts.ts
 import { Lusitana } from 'next/font/google';
 
-// Configura la fuente Lusitana
 const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
-  // Agrega más configuraciones según sea necesario
 });
 
-// Exporta la fuente Lusitana
 export { lusitana };

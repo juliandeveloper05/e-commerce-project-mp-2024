f959095c1690d47556f9ca522ea475033b537aff
juliandeveloper05 <juliansoto.dev@email.com>
2024-05-18 21:41:14 -0300

    Test Succesfully

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,24 +1,25 @@
-import { connectDb, disconnectDb } from '../../../utils/db';
-
-export async function GET() {
-  try {
-    await connectDb();
-    console.log('Conexión a MongoDB establecida');
-
-    const response = {
-      name: 'John Doe',
-    };
-
-    return new Response(JSON.stringify(response), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } catch (error) {
-    console.error('Error al conectar a MongoDB:', error);
-    throw error;
-  } finally {
-    await disconnectDb();
-  }
-}
+//import { connectDb, disconnectDb } from '../../../utils/db';
+//
+//export async function GET() {
+//  try {
+//    await connectDb();
+//    console.log('Conexión a MongoDB establecida');
+//
+//    const response = {
+//      name: 'John Doe',
+//    };
+//
+//    return new Response(JSON.stringify(response), {
+//      status: 200,
+//      headers: {
+//        'Content-Type': 'application/json',
+//      },
+//    });
+//  } catch (error) {
+//    console.error('Error al conectar a MongoDB:', error);
+//    throw error;
+//  } finally {
+//    await disconnectDb();
+//  }
+//}
+//

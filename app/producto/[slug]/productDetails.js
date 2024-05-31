a888d03be88cd07ee4a223db53bda0702f2ae294
juliandeveloper05 <juliansoto.dev@email.com>
2024-05-31 18:53:33 -0300

    Updated

--- app/producto/[slug]/productDetails.js
+++ app/producto/[slug]/productDetails.js
@@ -1,12 +1,5 @@
 'use client';
-import {
-  FaFacebookF,
-  FaTwitter,
-  FaInstagram,
-  FaLinkedinIn,
-  FaPinterest,
-  FaCartPlus,
-} from 'react-icons/fa';
+import { FaCartPlus } from 'react-icons/fa';
 import useSWR from 'swr';
 import ProductDetailsCarousel from './ProductDetailsCarousel';
 import rosecat1 from '../../../public/productCard/rosecat/rosecat1.jpg';
@@ -14,27 +7,10 @@ import rosecat2 from '../../../public/productCard/rosecat/rosecat2.jpg';
 import rosecat3 from '../../../public/productCard/rosecat/rosecat3.jpg';
 import Share from './Share/Share';
 
-interface ProductProps {
-  _id: string;
-  name: string;
-  slug: string;
-  price: number;
-  imageSrc: string;
-  imageSwiper: string[];
-  description?: string;
-}
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-interface ProductDetailsProps {
-  slug: string;
-}
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-const ProductDetails = ({ slug }: ProductDetailsProps) => {
-  const { data: product, error } = useSWR<ProductProps>(
-    `/api/products/${slug}`,
-    fetcher,
-  );
+const ProductDetails = ({ slug }) => {
+  const { data: product, error } = useSWR(`/api/products/${slug}`, fetcher);
 
   if (error) {
     return <div>Error al cargar el producto</div>;
@@ -52,14 +28,13 @@ const ProductDetails = ({ slug }: ProductDetailsProps) => {
           images={[rosecat1.src, rosecat2.src, rosecat3.src]}
         />
         <div>
-          {/* DESCRIPCIÓN */}
           <h1 className="mb-4 text-3xl font-bold">{product.name}</h1>
           <p className="mb-4 text-2xl font-semibold">${product.price}</p>
           <p className="mb-8 text-gray-700">
             {product.description || 'Sin descripción'}
           </p>
           <div className="mb-8 flex items-center">
-            {/* BOTONES */}
+            <div></div>
             <div className="mr-4 flex items-center rounded border">
               <button className="border-grey-500 rounded-l border-black bg-gray-200 px-4 py-3">
                 -
@@ -73,7 +48,6 @@ const ProductDetails = ({ slug }: ProductDetailsProps) => {
               <FaCartPlus className="mr-2" size={20} /> Agregar al Carrito
             </button>
           </div>
-          {/* REDES SOCIALES */}
           <div className="flex items-center border-t border-gray-300 pt-4">
             <div className="flex space-x-5">
               <Share />

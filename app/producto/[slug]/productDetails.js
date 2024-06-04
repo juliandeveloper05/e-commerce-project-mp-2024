e9c8b21011936d0d3457d0d1b05c5c9c9cfefc6a
juliandeveloper05 <juliansoto.dev@email.com>
2024-06-03 21:30:12 -0300

    Size Updated

--- app/producto/[slug]/productDetails.js
+++ app/producto/[slug]/productDetails.js
@@ -8,7 +8,6 @@ import rosecat3 from '../../../public/productCard/rosecat/rosecat3.jpg';
 import Share from './Share/Share';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import styles from './productdetailsstyles/productDetails.modules.scss';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -37,46 +36,49 @@ const ProductDetails = ({ slug }) => {
           <p className="mb-8 text-gray-700">
             {product.description || 'Sin descripción'}
           </p>
-          <div className="mb-8 flex items-center">
-            <div className={styles.infos__shipping}>
-              <h4>Select a size: </h4>
-              <div className={styles.infos__sizes_wrap}>
-                {product.sizes.map((size, i) => (
-                  <Link
-                    key={i}
-                    href={`/product/${product.slug}?style=${
-                      router?.query?.style ? router.query.style : ''
-                    }&size=${i}`}
+          <div className="mb-8">
+            <h4 className="mb-2 text-sm font-semibold text-gray-700">
+              Elige el tamaño:
+            </h4>
+            <div className="mb-4 flex space-x-2">
+              {product.sizes.map((size, i) => (
+                <Link
+                  key={i}
+                  href={`/product/${product.slug}?style=${
+                    router?.query?.style ? router.query.style : ''
+                  }&size=${i}`}
+                  className="group"
+                >
+                  <div
+                    className={`flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border text-gray-700 hover:bg-purple-600 hover:text-white ${
+                      i == (router?.query?.size ? router.query.size : '')
+                    }`}
                   >
-                    <div
-                      className={`${styles.infos__sizes_size} ${
-                        i == (router?.query?.size ? router.query.size : '') &&
-                        styles.active_size
-                      }`}
-                      onClick={() => setSize(size.size)}
-                    >
-                      {size.size}
-                    </div>
-                  </Link>
-                ))}
-              </div>
+                    {size.size}
+                  </div>
+                </Link>
+              ))}
             </div>
-            <div className="mr-4 flex items-center rounded border">
-              <button className="border-grey-500 rounded-l border-black bg-gray-200 px-4 py-3">
-                -
-              </button>
-              <span className="white border-white px-4 py-3">5</span>
-              <button className="white border-grey-500 rounded-r border-l bg-gray-200 px-4 py-3">
-                +
-              </button>
-            </div>
-            <button className="flex items-center rounded-md bg-purple-600 px-6 py-3 font-semibold text-white">
-              <FaCartPlus className="mr-2" size={20} /> Agregar al Carrito
-            </button>
           </div>
-          <div className="flex items-center border-t border-gray-300 pt-4">
-            <div className="flex space-x-5">
-              <Share />
+          <div>
+            <div className="mb-8 flex items-center">
+              <div className="mr-4 flex items-center rounded border">
+                <button className="border-grey-500 rounded-l border-black bg-gray-200 px-4 py-3">
+                  -
+                </button>
+                <span className="white border-white px-4 py-3">5</span>
+                <button className="white border-grey-500 rounded-r border-l bg-gray-200 px-4 py-3">
+                  +
+                </button>
+              </div>
+              <button className="flex items-center rounded-md bg-purple-600 px-6 py-3 font-semibold text-white">
+                <FaCartPlus className="mr-2" size={20} /> Agregar al Carrito
+              </button>
+            </div>
+            <div className="flex items-center border-t border-gray-300 pt-4">
+              <div className="flex space-x-5">
+                <Share />
+              </div>
             </div>
           </div>
         </div>

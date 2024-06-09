6cb94f73696020a3ec2dcc2c83264126b1c7ff1d
juliandeveloper05 <juliansoto.dev@email.com>
2024-06-09 20:32:27 -0300

    I've added effect hover from framer motion for the button of hero section

--- app/ui/Fancy-Button.tsx
+++ app/ui/Fancy-Button.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { FC, ReactNode } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 interface ButtonProps {
   text: string;
@@ -11,16 +14,22 @@ interface ButtonProps {
 const FancyButton: FC<ButtonProps> = ({ text, icon, onClick, children }) => {
   return (
     <div className="flex justify-center md:block">
-      <button
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileInView={{ scale: 1 }}
         className="fancy-btn text-1xl flex cursor-pointer items-center gap-2 rounded-full border-2 border-orange-500 bg-white px-2 py-3 font-bold text-black transition-all duration-75 hover:bg-orange-500 hover:text-white md:px-8 md:py-4"
         onClick={onClick}
       >
         {children}
         <span>{text}</span>
-        <span className="text-black transition-transform duration-100 group-hover:translate-x-[.23vw] group-hover:text-white">
+        <motion.span
+          whileHover={{ rotate: 90 }}
+          whileInView={{ rotate: 0 }}
+          className="text-black transition-transform duration-100 group-hover:translate-x-[.23vw] group-hover:text-white"
+        >
           <FaArrowRight />
-        </span>
-      </button>
+        </motion.span>
+      </motion.button>
     </div>
   );
 };

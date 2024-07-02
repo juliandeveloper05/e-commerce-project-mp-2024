e0b7902ced33a265af60bb830c5af83b72d0af9c
juliandeveloper05 <juliansoto.dev@email.com>
2024-07-02 15:56:41 -0300

    updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions, Session, User } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '../lib/db';
 import { Collection } from 'mongodb';
 
@@ -16,7 +15,6 @@ interface Account {
 }
 
 const authOptions: NextAuthOptions = {
-  adapter: MongoDBAdapter(clientPromise) as any, // Type assertion to avoid conflicts
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
@@ -45,22 +43,12 @@ const authOptions: NextAuthOptions = {
         });
 
         if (existingUser) {
-          console.log('Existing user found', existingUser);
-          if (
-            existingUser.accounts?.google?.providerAccountId !==
-            account?.providerAccountId
-          ) {
-            console.log('Updating existing user');
-            await userCollection.deleteOne({ email: user.email });
-            await createNewUser(userCollection, user, account);
-          } else {
-            console.log('Updating existing user');
-            await updateExistingUser(userCollection, user, account);
-          }
-        } else {
-          console.log('Creating new user');
-          await createNewUser(userCollection, user, account);
+          console.log('Existing user found, deleting');
+          await userCollection.deleteOne({ email: user.email });
         }
+
+        console.log('Creating new user');
+        await createNewUser(userCollection, user, account);
 
         console.log('SignIn process completed successfully');
         return true;
@@ -110,32 +98,9 @@ async function createNewUser(
           }
         : null,
     },
+    createdAt: new Date(),
   };
   await userCollection.insertOne(newUser);
-}
-
-async function updateExistingUser(
-  userCollection: Collection,
-  user: User,
-  account: Account | null,
-) {
-  const updateData: any = {
-    $set: {
-      name: user.name,
-      image: user.image,
-    },
-    $inc: { loginCount: 1 },
-  };
-
-  if (account) {
-    updateData.$set['accounts.google'] = {
-      providerAccountId: account.providerAccountId,
-      accessToken: account.access_token,
-      refreshToken: account.refresh_token,
-    };
-  }
-
-  await userCollection.updateOne({ email: user.email }, updateData);
 }
 
 const handler = NextAuth(authOptions);
